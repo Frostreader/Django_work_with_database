55c673dd8d13e8f34cb2e6014f2a5458bf0f08d0
Alexandr <gfrostreader@gmail.com>
2023-06-04 19:22:49 +0300

    initial commit

---
+++ phones/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from phones.models import Phone
+
+
+def index(request):
+    return redirect('catalog')
+
+
+def show_catalog(request):
+    sort_param = request.GET.get('sort')
+    if sort_param == 'name':
+        phones = Phone.objects.order_by('name')
+    elif sort_param == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif sort_param == 'max_price':
+        phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.all()
+
+    template = 'catalog.html'
+    context = {'phones': phones}
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
+    template = 'product.html'
+    context = {'phone': phone}
+    return render(request, template, context)
